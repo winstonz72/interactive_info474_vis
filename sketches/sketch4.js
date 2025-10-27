0f40a7893e2145c82638b4e3feaf9af6c58d9da2
Winston JR <winstonjr@Winstons-MacBook-Air.local>
2025-10-26 17:11:35 -0700

    Init Sketch4

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -1,14 +1,15 @@
-// Instance-mode sketch for tab 4
 registerSketch('sk4', function (p) {
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
-  };
-  p.draw = function () {
-    p.background(200, 240, 200);
-    p.fill(30, 120, 40);
-    p.textSize(32);
+    p.noStroke();
     p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. C', p.width / 2, p.height / 2);
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+
+  p.windowResized = function () {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
+
+  p.draw = function () {
+    p.background(235);
+  };
 });
